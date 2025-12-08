21e70c99d758b257859a5c8996fe000d16d0fe95
Raj31015 <154362399+Raj31015@users.noreply.github.com>
2025-12-08 20:07:34 +0530

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return <div className="flex items-center justify-center">
+    <div className="flex flex-col justify-center">
+        <SignIn />
+      <p>To demo app use</p>
+      <p>email: demouser@example.com</p>
+      <p>password:demouser</p>
+    </div>
+  
+  </div>
 }
